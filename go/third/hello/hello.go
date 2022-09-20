736d08f91721fb59f02b007219b8e8f84fe6c253
Amit Karpe <amitkarpe@gmail.com>
2022-09-20 17:31:01 +0800

    Return greetings for multiple people

---
+++ go/third/hello/hello.go
@@ -0,0 +1,27 @@
+// docs: https://go.dev/doc/tutorial/greetings-multiple-people
+// Return greetings for multiple people
+
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"example.com/greetings"
+)
+
+func main() {
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// message := greetings.Hello("Gladys")
+	// message, err := greetings.Hello("Amit")
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	messages, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(messages)
+}
